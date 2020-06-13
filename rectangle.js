3f24254582e23792f40cbfd65292e8090ff066c1
zainkhan10 <zain.khaan147@gmail.com>
2020-06-13 20:06:48 +0500

    callbacks and error handling

---
+++ rectangle.js
@@ -0,0 +1,13 @@
+module.exports = (x, y, callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(() =>
+            callback(new Error("Rectange dimensions should be greate than zero: l = " + x + " and b = " + y), null),
+            2000)
+    } else {
+        setTimeout(() =>
+            callback(null,  {
+                perimeter: () => (2 * (x + y)),
+                area: () => (x * y)
+            }), 2000)
+    }
+}
